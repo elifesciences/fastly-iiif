6b2f7f3b30469afe03c5bc77a705c1c1223a8b22
Chris Wilkinson <c.wilkinson@elifesciences.org>
2018-09-17 08:15:58 +0100

    Make tests more explicit

--- tests/v2.test.js
+++ tests/v2.test.js
@@ -3,8 +3,11 @@ import pathTool from 'path';
 import request from 'request-promise-native';
 import uuid from 'uuid/v1';
 
-const { URLSearchParams } = require('url');
+const domain = process.env.FASTLY_DOMAIN;
+const baseUrl = `https://${domain}`;
+const root = pathTool.resolve(__dirname, '../');
 
+// Fastly API client.
 const api = request.defaults({
   baseUrl: `https://api.fastly.com/service/${process.env.FASTLY_SERVICE_ID}/`,
   headers: {
@@ -13,8 +16,7 @@ const api = request.defaults({
   },
 });
 
-const domain = process.env.FASTLY_DOMAIN;
-const baseUrl = `https://${domain}`;
+// Test client.
 const http = request.defaults({
   baseUrl,
   headers: {
@@ -25,298 +27,286 @@ const http = request.defaults({
   simple: false,
 });
 
-const root = pathTool.resolve(__dirname, '../');
-
 beforeAll(async () => {
+  // Create a new version.
   const version = await api.post('version')
     .then(response => JSON.parse(response).number);
 
-  await Promise.all([
-    api.post(`version/${version}/backend`).form({
-      hostname: `${process.env.S3_BUCKET_NAME}.s3.amazonaws.com`,
-      name: 'bucket',
-      shield: 'dca-dc-us',
-    }),
-    api.post(`version/${version}/condition`).form({
-      name: 'cats-prefix',
-      statement: 'req.http.X-IIIF-Prefix == "kittehs"',
-      type: 'REQUEST',
-    }).then(() => api.post(`version/${version}/header`).form({
+  // Set up requests to configure the version.
+  const config = [];
+
+  // Set the backend to an S3 bucket containing test fixtures.
+  config.push(api.post(`version/${version}/backend`).form({
+    hostname: `${process.env.S3_BUCKET_NAME}.s3.amazonaws.com`,
+    name: 'bucket',
+    shield: 'dca-dc-us',
+  }));
+
+  // Set the domain.
+  config.push(api.post(`version/${version}/domain`).form({
+    name: domain,
+  }));
+
+  // Create a condition.
+  config.push(api.post(`version/${version}/condition`).form({
+    name: 'cats-prefix',
+    statement: 'req.http.X-IIIF-Prefix == "kittehs"',
+    type: 'REQUEST',
+  })
+    // Rewrite the prefix to match the bucket.
+    .then(() => api.post(`version/${version}/header`).form({
       name: 'cats-prefix',
       type: 'request',
       request_condition: 'cats-prefix',
       action: 'set',
       dst: 'url',
       src: '"/cats/" req.http.X-IIIF-Identifier "?" req.url.qs',
-    })),
-    api.post(`version/${version}/domain`).form({
-      name: domain,
-    }),
-    api.post(`version/${version}/header`).form({
-      name: 'vary-test-run',
-      type: 'cache',
-      action: 'append',
-      dst: 'http.Vary',
-      src: '"X-Test-Run"',
-    }),
-    api.post(`version/${version}/snippet`).form({
-      name: 'IIIF config',
-      dynamic: 0,
-      type: 'init',
-      content: `
+    })));
+
+  // Make sure all responses have 'Vary: X-Test-Run' to avoid conflicts between test runs.
+  config.push(api.post(`version/${version}/header`).form({
+    name: 'vary-test-run',
+    type: 'cache',
+    action: 'append',
+    dst: 'http.Vary',
+    src: '"X-Test-Run"',
+  }));
+
+  // Add 'iiif_config' to configure the VCL.
+  config.push(api.post(`version/${version}/snippet`).form({
+    name: 'IIIF config',
+    dynamic: 0,
+    type: 'init',
+    content: `
 sub iiif_config {
 
   set req.http.X-IIIF-Version = if(req.http.X-Test-IIIF-Version, req.http.X-Test-IIIF-Version, "2");
 
 }
 `,
-    }),
-    fs.readFile(`${root}/iiif.vcl`)
-      .then((contents) => {
-        api.post(`version/${version}/vcl`).form({
-          name: 'IIIF',
-          content: contents,
-          main: true,
-        });
-      }),
-  ]);
+  }));
 
-  return api.put(`version/${version}/activate`);
+  // Add the VCL.
+  config.push(fs.readFile(`${root}/iiif.vcl`)
+    .then((contents) => {
+      api.post(`version/${version}/vcl`).form({
+        name: 'IIIF',
+        content: contents,
+        main: true,
+      });
+    }));
+
+  // Send configuration requests.
+  await Promise.all(config);
+
+  // Activate the version.
+  await api.put(`version/${version}/activate`);
 });
 
-const imageUri = (originalParts) => {
-  const parts = Object.assign({
-    prefix: '',
-    identifier: 'pug-life.jpg',
-    region: 'full',
-    size: 'full',
-    rotation: 0,
-    quality: 'default',
-    format: 'jpg',
-  }, originalParts);
-
-  return `${parts.prefix}/${parts.identifier}/${parts.region}/${parts.size}/${parts.rotation}/${parts.quality}.${parts.format}`;
-};
+const createImageUri = ({
+  prefix = '',
+  identifier = 'pug-life.jpg',
+  region = 'full',
+  size = 'full',
+  rotation = 0,
+  quality = 'default',
+  format = 'jpg',
+} = {}) => `${prefix}/${identifier}/${region}/${size}/${rotation}/${quality}.${format}`;
 
 describe('Image request', () => {
-  const ok = async (iiifParameters, requestedIoParameters = {}, method = 'GET') => {
-    const ioParameters = Object.assign({
-      format: 'pjpg',
-    }, requestedIoParameters);
+  describe('HTTP methods', () => {
+    const supported = ['GET', 'HEAD'];
+    const unsupported = ['DELETE', 'OPTIONS', 'PATCH', 'POST', 'PUT', 'TRACE'];
+    const invalid = ['FOO'];
 
-    const ioQueryParameters = new URLSearchParams(ioParameters);
+    describe('Supported values', () => {
+      test.each(supported)('%s', async (method) => {
+        const response = await http({ method, uri: createImageUri() });
 
-    const response = await http({ method, uri: imageUri(iiifParameters) });
-
-    expect(response.statusCode).toBe(200);
-    expect(response.headers['x-fastly-io-url']).toBe(`/pug-life.jpg?${ioQueryParameters.toString()}`);
-  };
-
-  const error = async (iiifParameters, statusCode, method = 'GET') => {
-    const response = await http({ method, uri: imageUri(iiifParameters) });
-
-    expect(response.statusCode).toBe(statusCode);
-  };
-
-  const badRequest = iiifParameters => error(iiifParameters, 400);
-
-  describe('Methods', () => {
-    describe('Supported', () => {
-      const methods = [
-        'GET',
-        'HEAD',
-      ];
-
-      test.each(methods)('%s', method => ok({}, {}, method));
+        expect(response.statusCode).toBe(200);
+        expect(response.headers['x-fastly-io-url']).toBe('/pug-life.jpg?format=pjpg');
+      });
     });
 
-    describe('Unsupported', () => {
-      const methods = [
-        'DELETE',
-        'OPTIONS',
-        'PATCH',
-        'POST',
-        'PUT',
-        'TRACE',
-      ];
+    describe('Unsupported values', () => {
+      test.each(unsupported)('%s', async (method) => {
+        const response = await http({ method, uri: createImageUri() });
 
-      test.each(methods)('%s', method => error({}, 405, method));
+        expect(response.statusCode).toBe(405);
+      });
     });
 
-    describe('Invalid', () => {
-      const methods = [
-        'FOO',
-      ];
+    describe('Invalid values', () => {
+      test.each(invalid)('%s', async (method) => {
+        const response = await http({ method, uri: createImageUri() });
 
-      test.each(methods)('%s', method => error({}, 405, method));
+        expect(response.statusCode).toBe(405);
+      });
     });
   });
 
-  describe('Region', () => {
-    describe('Supported', () => {
-      const regions = [
-        'full',
-      ];
+  describe('Region parameter', () => {
+    const supported = [
+      ['full', '/pug-life.jpg?format=pjpg'],
+    ];
+    const unsupported = ['square', '0,0,100,100', '100,100,100,100', 'pct:1,2,3,4'];
+    const invalid = ['-1,-1,100,100', '0,0,-1,-1', 'pct:1,2', 'pct:1,2,3', 'pct:1,2,3,0', 'foo'];
 
-      test.each(regions)('%s', region => ok({ region }));
+    describe('Supported values', () => {
+      test.each(supported)('%s', async (region, expected) => {
+        const response = await http({ uri: createImageUri({ region }) });
+
+        expect(response.statusCode).toBe(200);
+        expect(response.headers['x-fastly-io-url']).toBe(expected);
+      });
     });
 
-    describe('Unsupported', () => {
-      const regions = [
-        'square',
-        '0,0,100,100',
-        '100,100,100,100',
-        'pct:1,2,3,4',
-      ];
+    describe('Unsupported values', () => {
+      test.each(unsupported)('%s', async (region) => {
+        const response = await http({ uri: createImageUri({ region }) });
 
-      test.each(regions)('%s', region => badRequest({ region }));
+        expect(response.statusCode).toBe(400);
+      });
     });
 
-    describe('Invalid', () => {
-      const regions = [
-        '-1,-1,100,100',
-        '0,0,-1,-1',
-        'pct:1,2',
-        'pct:1,2,3',
-        'pct:1,2,3,0',
-        'foo',
-      ];
+    describe('Invalid values', () => {
+      test.each(invalid)('%s', async (region) => {
+        const response = await http({ uri: createImageUri({ region }) });
 
-      test.each(regions)('%s', region => badRequest({ region }));
-    });
-  });
-
-  describe('Size', () => {
-    describe('Supported', () => {
-      const sizes = [
-        'full',
-      ];
-
-      test.each(sizes)('%s', size => ok({ size }));
-    });
-
-    describe('Unsupported', () => {
-      const sizes = [
-        'max',
-        '100,',
-        ',100',
-        '100,100',
-        '!100,100',
-        'pct:50',
-        'pct:100',
-        '!1',
-        '!90',
-      ];
-
-      test.each(sizes)('%s', size => badRequest({ size }));
-    });
-
-    describe('Invalid', () => {
-      const sizes = [
-        '0,0',
-        '-1,-1',
-        'pct:0',
-        'pct:101',
-        'foo',
-      ];
-
-      test.each(sizes)('%s', size => badRequest({ size }));
+        expect(response.statusCode).toBe(400);
+      });
     });
   });
 
-  describe('Rotation', () => {
-    describe('Supported', () => {
-      const rotations = [
-        '0',
-      ];
+  describe('Size parameter', () => {
+    const supported = [
+      ['full', '/pug-life.jpg?format=pjpg'],
+    ];
+    const unsupported = ['max', '100,', ',100', '100,100', '!100,100', 'pct:50', 'pct:100', '!1', '!90'];
+    const invalid = ['0,0', '-1,-1', 'pct:0', 'pct:101', 'foo'];
 
-      test.each(rotations)('%s', rotation => ok({ rotation }));
+    describe('Supported values', () => {
+      test.each(supported)('%s', async (size, expected) => {
+        const response = await http({ uri: createImageUri({ size }) });
+
+        expect(response.statusCode).toBe(200);
+        expect(response.headers['x-fastly-io-url']).toBe(expected);
+      });
     });
 
-    describe('Unsupported', () => {
-      const rotations = [
-        '90',
-        '180',
-        '270',
-        '360',
-        '1',
-        '1.5',
-        '!0',
-        '!1',
-        '!1.5',
-        '!90',
-      ];
+    describe('Unsupported values', () => {
+      test.each(unsupported)('%s', async (size) => {
+        const response = await http({ uri: createImageUri({ size }) });
 
-      test.each(rotations)('%s', rotation => badRequest({ rotation }));
+        expect(response.statusCode).toBe(400);
+      });
     });
 
-    describe('Invalid', () => {
-      const rotations = [
-        '-0',
-        '-90',
-        'foo',
-      ];
+    describe('Invalid values', () => {
+      test.each(invalid)('%s', async (size) => {
+        const response = await http({ uri: createImageUri({ size }) });
 
-      test.each(rotations)('%s', rotation => badRequest({ rotation }));
+        expect(response.statusCode).toBe(400);
+      });
     });
   });
 
-  describe('Quality', () => {
-    describe('Supported', () => {
-      const qualities = [
-        'default',
-      ];
+  describe('Rotation parameter', () => {
+    const supported = [
+      ['0', '/pug-life.jpg?format=pjpg'],
+    ];
+    const unsupported = ['90', '180', '270', '360', '1', '1.5', '!0', '!1', '!1.5', '!90'];
+    const invalid = ['-0', '-90', 'foo'];
 
-      test.each(qualities)('%s', quality => ok({ quality }));
+    describe('Supported values', () => {
+      test.each(supported)('%s', async (rotation, expected) => {
+        const response = await http({ uri: createImageUri({ rotation }) });
+
+        expect(response.statusCode).toBe(200);
+        expect(response.headers['x-fastly-io-url']).toBe(expected);
+      });
     });
 
-    describe('Unsupported', () => {
-      const qualities = [
-        'bitonal',
-        'color',
-        'gray',
-      ];
+    describe('Unsupported values', () => {
+      test.each(unsupported)('%s', async (rotation) => {
+        const response = await http({ uri: createImageUri({ rotation }) });
 
-      test.each(qualities)('%s', quality => badRequest({ quality }));
+        expect(response.statusCode).toBe(400);
+      });
     });
 
-    describe('Invalid', () => {
-      const qualities = [
-        'foo',
-      ];
+    describe('Invalid values', () => {
+      test.each(invalid)('%s', async (rotation) => {
+        const response = await http({ uri: createImageUri({ rotation }) });
 
-      test.each(qualities)('%s', quality => badRequest({ quality }));
+        expect(response.statusCode).toBe(400);
+      });
     });
   });
 
-  describe('Format', () => {
-    describe('Supported', () => {
-      const formats = {
-        jpg: 'pjpg',
-      };
+  describe('Quality parameter', () => {
+    const supported = [
+      ['default', '/pug-life.jpg?format=pjpg'],
+    ];
+    const unsupported = ['bitonal', 'color', 'gray'];
+    const invalid = ['foo'];
 
-      test.each(Object.entries(formats))('%s', (iiifFormat, ioFormat) => ok({ format: iiifFormat }, { format: ioFormat }));
+    describe('Supported values', () => {
+      test.each(supported)('%s', async (quality, expected) => {
+        const response = await http({ uri: createImageUri({ quality }) });
+
+        expect(response.statusCode).toBe(200);
+        expect(response.headers['x-fastly-io-url']).toBe(expected);
+      });
     });
 
-    describe('Unsupported', () => {
-      const formats = [
-        'gif',
-        'jp2',
-        'pdf',
-        'png',
-        'tif',
-        'webp',
-      ];
+    describe('Unsupported values', () => {
+      test.each(unsupported)('%s', async (quality) => {
+        const response = await http({ uri: createImageUri({ quality }) });
 
-      test.each(formats)('%s', format => badRequest({ format }));
+        expect(response.statusCode).toBe(400);
+      });
     });
 
-    describe('Invalid', () => {
-      const formats = [
-        'foo',
-      ];
+    describe('Invalid values', () => {
+      test.each(invalid)('%s', async (quality) => {
+        const response = await http({ uri: createImageUri({ quality }) });
 
-      test.each(formats)('%s', format => badRequest({ format }));
+        expect(response.statusCode).toBe(400);
+      });
+    });
+  });
+
+  describe('Format parameter', () => {
+    const supported = [
+      ['jpg', '/pug-life.jpg?format=pjpg'],
+    ];
+    const unsupported = ['gif', 'jp2', 'pdf', 'png', 'tif', 'webp'];
+    const invalid = ['foo'];
+
+    describe('Supported values', () => {
+      test.each(supported)('%s', async (format, expected) => {
+        const response = await http({ uri: createImageUri({ format }) });
+
+        expect(response.statusCode).toBe(200);
+        expect(response.headers['x-fastly-io-url']).toBe(expected);
+      });
+    });
+
+    describe('Unsupported values', () => {
+      test.each(unsupported)('%s', async (format) => {
+        const response = await http({ uri: createImageUri({ format }) });
+
+        expect(response.statusCode).toBe(400);
+      });
+    });
+
+    describe('Invalid values', () => {
+      test.each(invalid)('%s', async (format) => {
+        const response = await http({ uri: createImageUri({ format }) });
+
+        expect(response.statusCode).toBe(400);
+      });
     });
   });
 });
@@ -350,7 +340,7 @@ describe('Info request', () => {
   ])('%s/%s/info.json', async (prefix, identifier, requiredJson) => {
     const json = Object.assign({
       '@context': 'http://iiif.io/api/image/2/context.json',
-      '@id': baseUrl.concat(prefix, '/', identifier),
+      '@id': `${baseUrl}${prefix}/${identifier}`,
       protocol: 'http://iiif.io/api/image',
       profile: [
         'http://iiif.io/api/image/2/level0.json',
@@ -419,7 +409,7 @@ describe('Unknown versions', () => {
   ];
 
   test.each(paths)('%s', async (version) => {
-    const response = await http.get(imageUri(), { headers: { 'X-Test-IIIF-Version': version } });
+    const response = await http.get(createImageUri(), { headers: { 'X-Test-IIIF-Version': version } });
 
     expect(response.statusCode).toBe(500);
   });
